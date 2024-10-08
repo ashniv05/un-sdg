5e6c846696fd64a686aa053d0040f9f977e37827
xTyten <tylerrenzejan@gmail.com>
2024-10-08 19:15:54 -0400

    colorOnly, stateful demonstration

--- src/un-sdg.js
+++ src/un-sdg.js
@@ -10,14 +10,15 @@ export class unSdg extends DDDSuper(LitElement) {
 
   constructor() {
     super();
-    this.goal = 1;
-    this.imgSrc = "./lib/svgs/goal-1.svg";
+    this.goal = "circle";
+    this.imgSrc = "./lib/svgs/circle.png";
     this.width = "254px";
     this.height = "254px";
-    this.label = "Goal 1: No poverty";
+    this.label = "Sustainable developments logo";
     this.loading = "lazy"
     this.fetchPriority = "low";
-    this.colorOnly = false;
+    this.colorOnly = "false";
+    this.isImageVisible = "true";
   }
 
   static get properties() {
@@ -29,7 +30,8 @@ export class unSdg extends DDDSuper(LitElement) {
       label: { type: String },
       loading: { type: String },
       fetchPriority: { type: String },
-      colorOnly: { type: Boolean },
+      colorOnly: { type: String },
+      isImageVisible: { type: String },
     };
   }
 
@@ -44,7 +46,26 @@ export class unSdg extends DDDSuper(LitElement) {
         font-family: var(--ddd-font-navigation);
         font-size: var(--un-sdg-font-size, var(--ddd-font-size-s));
         */
-
+        width: var(--width, 254px);
+        height: var(--height, 254px);
+        background-color: white;
+        --un-sdg-goal-1: rgb(235, 28, 44);
+        --un-sdg-goal-2: rgb(210, 160, 42);
+        --un-sdg-goal-3: rgb(44, 155, 72);
+        --un-sdg-goal-4: rgb(194, 31, 51);
+        --un-sdg-goal-5: rgb(239, 64, 42);
+        --un-sdg-goal-6: rgb(0, 173, 216);
+        --un-sdg-goal-7: rgb(253, 183, 19);
+        --un-sdg-goal-8: rgb(143, 23, 55);
+        --un-sdg-goal-9: rgb(243, 109, 36);
+        --un-sdg-goal-10: rgb(224, 21, 131);
+        --un-sdg-goal-11: rgb(249, 157, 37);
+        --un-sdg-goal-12: rgb(207, 141, 42);
+        --un-sdg-goal-13: rgb(72, 119, 61);
+        --un-sdg-goal-14: rgb(0, 125, 187);
+        --un-sdg-goal-15: rgb(63, 175, 73);
+        --un-sdg-goal-16: rgb(1, 85, 138);
+        --un-sdg-goal-17: rgb(25, 54, 103);
       }
       .svg-wrapper {
         /*
@@ -54,9 +75,12 @@ export class unSdg extends DDDSuper(LitElement) {
         padding: 0;
         margin: 0;
       }
+      .hidden {
+        visibility: hidden;
+      }
       img {
-        width: var(--width, 254px);
-        height: var(--height, 254px);
+        width: 100%;
+        height: 100%;
       }
     `];
   }
@@ -70,9 +94,13 @@ export class unSdg extends DDDSuper(LitElement) {
     }
     if (changedProperties.has('goal')) {
       this.updateImageandAlt();
-    }
-    if (changedProperties.has('colorOnly')) {
       this.updateColor();
+    } 
+    if (changedProperties.has('colorOnly') && this.colorOnly == "true") {
+      this.isImageVisible = false;
+    }
+    else if (changedProperties.has('colorOnly') && this.colorOnly == "false") {
+      this.isImageVisible = true;
     }
     if (changedProperties.has('loading')) {
       const loading = this.getAttribute('loading');
@@ -98,8 +126,61 @@ export class unSdg extends DDDSuper(LitElement) {
     }
   }
 
-  updateColor() {
-
+  updateColor() { 
+    const goal = this.getAttribute('goal'); 
+    switch (goal) {
+      case '1':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-1, white)');
+        break;
+      case '2':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-2, white)');
+        break;
+      case '3':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-3, white)');
+        break;
+      case '4':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-4, white)');
+        break;
+      case '5':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-5, white)');
+        break;
+      case '6':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-6, white)');
+        break;
+      case '7':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-7, white)');
+        break;
+      case '8':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-8, white)');
+        break;
+      case '9':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-9, white)');
+        break;
+      case '10':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-10, white)');
+        break;
+      case '11':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-11, white)');
+        break;
+      case '12':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-12, white)');
+        break;
+      case '13':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-13, white)');
+        break;
+      case '14':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-14, white)');
+        break;
+      case '15':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-15, white)');
+        break;
+      case '16':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-16, white)');
+        break;
+      case '17':
+        this.style.setProperty('background-color', 'var(--un-sdg-goal-17, white)');
+        break;
+    }  
   }
   updateImageandAlt() {
     const goal = this.getAttribute('goal');
@@ -183,10 +264,13 @@ export class unSdg extends DDDSuper(LitElement) {
     }
   }
 
+  // class="${this.isImageVisible ? '' : 'hidden'}"
+  // class="${this.isImageVisible ? valueIfTrue : valueIfFalse}"
   render() {
     return html`
       <div class="svg-wrapper">
         <img 
+        class="${this.isImageVisible ? '' : 'hidden'}"
         src="${this.imgSrc}"
         alt="${this.label}"
         loading="${this.loading}"
