35cf860d3052a1b161aecef7eb0d969bb51f97e8
xTyten <44031891+xTyten@users.noreply.github.com>
2024-10-10 16:23:18 -0400

    Update un-sdg.js

--- src/un-sdg.js
+++ src/un-sdg.js
@@ -147,14 +147,14 @@ export class unSdg extends DDDSuper(LitElement) {
 
   render() {
     // Sets the image source according to what number the goal is
-    let imgSrc = new URL(`/svgs/goal-${this.goal}.svg`, import.meta.url).href;
+    let imgSrc = new URL(`./svgs/goal-${this.goal}.svg`, import.meta.url).href;
     // If the goal is all, set it to the all svg
     if (this.goal === 'all') {
-      imgSrc = new URL(`/svgs/${this.goal}.svg`, import.meta.url).href;
+      imgSrc = new URL(`./svgs/${this.goal}.svg`, import.meta.url).href;
     }
     // If the goal is circle, set it to the circle image
     else if (this.goal === 'circle') {
-      imgSrc = new URL(`/svgs/${this.goal}.png`, import.meta.url).href;
+      imgSrc = new URL(`./svgs/${this.goal}.png`, import.meta.url).href;
     }
 
     return html`
